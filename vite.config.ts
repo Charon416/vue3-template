22ab16ccd448341f88852b333bad1810ca04ce9b
Charon416 <hanfeng0416@gmail.com>
2024-01-21 00:55:45 +0800

    feat: 配置系统代理

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { ConfigEnv, UserConfig, defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
@@ -9,25 +9,39 @@ import { resolve } from 'path';
 const CWD = process.cwd();
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    createSvgIconsPlugin({
-      // Specify the icon folder to be cached
-      iconDirs: [resolve(CWD, 'src/assets/svg')],
-      // Specify symbolId format
-      symbolId: 'svg-icon-[dir]-[name]'
-    }),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve('./src')
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [
+      vue(),
+      createSvgIconsPlugin({
+        // Specify the icon folder to be cached
+        iconDirs: [resolve(CWD, 'src/assets/svg')],
+        // Specify symbolId format
+        symbolId: 'svg-icon-[dir]-[name]'
+      }),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      })
+    ],
+    server: {
+      open: true, // 运行是否自动打开浏览器
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          target: 'http://vapi.youlai.tech/api/v1',
+          changeOrigin: true,
+          // '/dev-api/auth/captcha => 'http://vapi.youlai.tech/api/v1/auth/captcha'
+          rewrite: path => path.replace(new RegExp('^' + env.VITE_APP_BASE_API), '')
+        }
+      }
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve('./src')
+      }
     }
   }
 })
