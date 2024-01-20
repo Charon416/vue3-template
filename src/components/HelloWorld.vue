d091663478d9d0e1908e1911ddd1b2d551d93608
Charon416 <hanfeng0416@gmail.com>
2024-01-20 21:24:50 +0800

    feat: 安装element-plus并配置按需引入

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,6 +7,14 @@ const count = ref(0)
 </script>
 
 <template>
+  <el-row class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </el-row>
   <h1>{{ msg }}</h1>
 
   <div class="card">
