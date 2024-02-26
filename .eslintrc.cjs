aa72d8841c8da1662a47052f6b5c01abb327e673
lan <hanfeng0416@gmail.com>
2024-02-26 18:29:43 +0800

    feat: :sparkles: 完善提交规范

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,38 +1,30 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:vue/vue3-essential"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:vue/vue3-essential"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parser": "vue-eslint-parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "parser": "@typescript-eslint/parser",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "vue"
-    ],
-    "rules": {
-        "vue/multi-word-component-names": "off", // 禁用组件名称规则
-        "@typescript-eslint/no-explicit-any": "off" // 解除any类型限制
-    }
-}
+  ],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "vue"],
+  rules: {
+    "vue/multi-word-component-names": "off", // 禁用组件名称规则
+    "@typescript-eslint/no-explicit-any": "off", // 解除any类型限制
+    "no-undef": "off", // 接触警告'module' is not defined.eslintno-undef
+  },
+};
